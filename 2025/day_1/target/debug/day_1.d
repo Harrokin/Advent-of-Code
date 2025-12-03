f41710b0a6344b407c3c1dc22e590e5fc068c5c8
Alex Guess <AGuess@metrol.co.uk>
2025-12-03 17:53:19 +0000

    Added AoC 2025 folders and skeleton of day 1

---
+++ 2025/day_1/target/debug/day_1.d
@@ -0,0 +1,1 @@
+C:\Users\AGuess\Documents\GitHub\Advent-of-Code\2025\day_1\target\debug\day_1.exe: C:\Users\AGuess\Documents\GitHub\Advent-of-Code\2025\day_1\src\main.rs
---
+++ 2025/day_1/target/debug/deps/day_1.d
@@ -0,0 +1,5 @@
+C:\Users\AGuess\Documents\GitHub\Advent-of-Code\2025\day_1\target\debug\deps\day_1.exe: src/main.rs
+
+C:\Users\AGuess\Documents\GitHub\Advent-of-Code\2025\day_1\target\debug\deps\day_1.d: src/main.rs
+
+src/main.rs:
